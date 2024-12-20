d7564373d6dd9e692636032971a79d0897c5fd51
Abedsoona <abedsoona63@gmail.com>
2024-12-20 16:08:55 +0000

    Hello.c update

--- Hello/hello.c
+++ Hello/hello.c
@@ -2,11 +2,26 @@
 
 
 // main function - the entry point
-
-
+// the return value of the main is an integer
 int main()
 {
+       // print message "Hello World!!!" to the console
+       // \n is the shortcut for new line
      printf("Hello World!!!\n\n\n");
+
+     // variable of 32 chars
+     char name[32];
+
+     // ask for a name
+     printf("Enter a name: ");
+
+     // read the input of the user
+     scanf("%s", name);
+
+     // print a message with the same entered
+     printf("\nHello \t%s\n\n\n", name);
+     /* return 0: indicated that the program is terminated
+        successfully */
 
 return 0;
 }
